b5e3ff831e964736d572a603ac83ffc33d8b1026
Matias Duhalde <matias.duhalde@uc.cl>
2022-05-06 10:49:03 -0400

    :sparkles: Add expiration completed event

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export * from './middlewares/validate-request';
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
+export * from './events/expiration-complete-event';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/order-created-event';
